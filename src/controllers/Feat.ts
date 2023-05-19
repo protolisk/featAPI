cf9f91c677d07d9ac0cb24f5e9e1d88075257b3d
protolisk <protoliskk@gmail.com>
2023-05-19 07:09:11 +0200

    discord webhook

--- src/controllers/Feat.ts
+++ src/controllers/Feat.ts
@@ -1,6 +1,21 @@
 import { NextFunction, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import Feat from '../models/Feat'; //Feat? or feat?
+import axios from 'axios';
+
+const sendWebhookMessage = (featName: string) => {
+    const webhookUrl = 'https://discord.com/api/webhooks/1108726527127978036/cQkMNMU1Gt0MWdeVR-U_m6xtzwvqP_NzbBmMIPiKrhB6Ljfu4o5iyoCCG5KmqQFRxIKh';
+    const message = `New feat added: ${featName}`;
+
+    axios
+        .post(webhookUrl, { content: message })
+        .then((response) => {
+            console.log('Webhook message sent successfully');
+        })
+        .catch((error) => {
+            console.error('Failed to send webhook message:', error);
+        });
+};
 
 const createFeat = (req: Request, res: Response, next: NextFunction) => {
     const { name, flavortext, prereq, benefit, source } = req.body;
@@ -15,7 +30,10 @@ const createFeat = (req: Request, res: Response, next: NextFunction) => {
     });
     return feat
         .save()
-        .then((feat) => res.status(201).json({ feat }))
+        .then((feat) => {
+            sendWebhookMessage(feat.name); // Send webhook message with feat name
+            res.status(201).json({ feat });
+        })
         .catch((error) => res.status(500).json({ error }));
 };
 const readFeatID = (req: Request, res: Response, next: NextFunction) => {
